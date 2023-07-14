41335bb7d5e60c318e8ad37da173ae1033fee79b
gkz <giorgadzekonstantin@gmail.com>
2023-07-14 19:33:09 +0400

    Add all tests

--- src/test/java/ru/netology/qa65/RadioTest.java
+++ src/test/java/ru/netology/qa65/RadioTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class RadioTest {
+// ТЕСТЫ ГРОМКОСТЬ --------------------------------------------------------
 
-    @Test  // ПРОВЕРКА ВЫСТАВЛЕНИЯ ГРОМКОСТИ
+
+    @Test  // ВЫСТАВЛЕНИЕ ГРОМКОСТИ
 
     public void shouldSetVolume() {
         Radio radio = new Radio();
@@ -18,7 +20,7 @@ public class RadioTest {
 
     }
 
-    @Test  // ПРОВЕРКА ПРИБАВЛЕНИЯ ГРОМКОСТИ
+    @Test  // ПРИБАВЛЕНИЕ ГРОМКОСТИ
 
     public void testIncreaseVolume() {
         Radio radio = new Radio();
@@ -32,7 +34,7 @@ public class RadioTest {
 
     }
 
-    @Test  // ПРОВЕРКА УМЕНЬШЕНИЯ ГРОМКОСТИ
+    @Test  // УМЕНЬШЕНИЕ ГРОМКОСТИ
 
     public void testReduceVolume() {
         Radio radio = new Radio();
@@ -46,6 +48,64 @@ public class RadioTest {
 
     }
 
+    @Test  // ПРИБАВЛЕНИЯ ГРОМКОСТИ ПОГРАНИЧНЫЕ ЗНАЧЕНИЯ
+
+    public void testIncreaseVolumeMax() {
+        Radio radio = new Radio();
+
+        int expected = 100;
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test  // УМЕНЬШЕНИЕ ГРОМКОСТИ ПОГРАНИЧНЫЕ ЗНАЧЕНИЯ
+
+    public void testReduceVolumeMin() {
+        Radio radio = new Radio();
+
+        int expected = 0;
+        radio.setCurrentVolume(0);
+        radio.reduceVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test  // ВЫСТАВЛЕНИЕ ГРОМКОСТИ ВЫШЕ ГРАНИЦ
+
+    public void shouldSetVolumeAboveMax() {
+        Radio radio = new Radio();
+
+        int expected = 100;
+        radio.setCurrentVolume(101);
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test  // ВЫСТАВЛЕНИЕ ГРОМКОСТИ НИЖЕ ГРАНИЦ
+
+    public void shouldSetVolumeBelowMax() {
+        Radio radio = new Radio();
+
+        int expected = 0;
+        radio.setCurrentVolume(-1);
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+// ТЕСТЫ СТАНЦИИ --------------------------------------------------------
+
+
     @Test // ПРОВЕРКА ВЫСТАВЛЕНИЯ СТАНЦИИ
 
     public void shouldSetStation () {
@@ -58,30 +118,79 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test // ПЕРЕУЛЮЧЕНИЕ СТАНЦИИ ВПЕРЕД
+    @Test // ПЕРЕКЛЮЧЕНИЕ СТАНЦИИ ВПЕРЕД
 
     public void testNextStation () {
         Radio radio = new Radio();
 
-        int expected =6;
-        radio.setCurrentStation(5);
+        int expected =5;
+        radio.setCurrentStation(4);
         radio.nextStation();
         int actual = radio.currentStation;
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test // ПЕРЕУЛЮЧЕНИЕ СТАНЦИИ НАЗАД
+    @Test // ПЕРЕКЛЮЧЕНИЕ СТАНЦИИ НАЗАД
 
     public void testPreviewStation () {
         Radio radio = new Radio();
 
-        int expected =4;
-        radio.setCurrentStation(5);
+        int expected =3;
+        radio.setCurrentStation(4);
         radio.previewStation();
         int actual = radio.currentStation;
 
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test // ПЕРЕКЛЮЧЕНИЕ СТАНЦИИ ВПЕРЕД ПОГРАНИЧНОЕ ЗНАЧЕНИЕ
+
+    public void testNextStationMax () {
+        Radio radio = new Radio();
+
+        int expected =0;
+        radio.setCurrentStation(9);
+        radio.nextStation();
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // ПЕРЕКЛЮЧЕНИЕ СТАНЦИИ НАЗАД ПОГРАНИЧНОЕ ЗНАЧЕНИЕ
+
+    public void testPreviewStationMin () {
+        Radio radio = new Radio();
+
+        int expected =9;
+        radio.setCurrentStation(0);
+        radio.previewStation();
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // ПРОВЕРКА ВЫСТАВЛЕНИЯ СТАНЦИИ ВЫШЕ ГРАНИЦ
+
+    public void shouldSetStationAboveMax () {
+        Radio radio = new Radio();
+
+        int expected =9;
+        radio.setCurrentStation(10);
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // ПРОВЕРКА ВЫСТАВЛЕНИЯ СТАНЦИИ НИЖЕ ГРАНИЦ
+
+    public void shouldSetStationBelowMax () {
+        Radio radio = new Radio();
+
+        int expected =0;
+        radio.setCurrentStation(-1);
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
