576a00e63f01e47d6b721ecdd0c34893ab609bca
gkz <giorgadzekonstantin@gmail.com>
2023-07-14 16:11:14 +0400

    Add Increase and reduce Volume and tests

--- src/main/java/ru/netology/qa65/Radio.java
+++ src/main/java/ru/netology/qa65/Radio.java
@@ -1,22 +1,42 @@
 package ru.netology.qa65;
 
 public class Radio {
-    public int currentVolume;
-    public int currentStation;
+    public int currentVolume; // значение громкости
+    public int currentStation; // значение станции
 
-    public int getCurrentVolume () {
+    public int getCurrentVolume() {
         return currentVolume;
-    }
-    public int getCurrentStation () {
+    }  // узнать громкость
+
+    public int getCurrentStation() {
         return currentStation;
+    } // узнать станцию
+
+    public void setCurrentVolume(int newCurrentVolume) { // Выставить громкость
+        if (newCurrentVolume > 100) {
+            currentVolume = 100;
+        }
+        else if (newCurrentVolume < 0) {
+            currentVolume = 0;
+        } else {
+            currentVolume = newCurrentVolume;
+        }
     }
 
-    public int setCurrentVolume (int newCurrentVolume) {
-        return currentVolume = newCurrentVolume;
+    public void increaseVolume () { // Увеличение громкости
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
     }
 
-    public int setCurrentStation () {
-        return currentStation;
+    public void reduceVolume () { // Уменьшение громкости
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
+    public int setCurrentStation(int newCurrentStation) { // выставить станцию
+        return currentStation = newCurrentStation;
     }
 
 }
--- src/test/java/ru/netology/qa65/RadioTest.java
+++ src/test/java/ru/netology/qa65/RadioTest.java
@@ -3,31 +3,59 @@ package ru.netology.qa65;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-    public class RadioTest {
+public class RadioTest {
 
-        @Test
-        public void testSetCurrentVolume() {
-            Radio radio = new Radio();
+    @Test  // ПРОВЕРКА ВЫСТАВЛЕНИЯ ГРОМКОСТИ
 
-            int expectedVolume = 50;
-            radio.setCurrentVolume(expectedVolume);
+    public void shouldSetVolume() {
+        Radio radio = new Radio();
 
-            int actualVolume = radio.getCurrentVolume();
+        int expected = 50;
+        radio.setCurrentVolume(50);
+        int actual = radio.getCurrentVolume();
 
-            Assertions.assertEquals(expectedVolume, actualVolume);
-        }
-
-
-    @Test
-        public void shouldSetVolume () {
-            Radio radio =new Radio();
-
-            int expected = 50;
-            radio.setCurrentVolume(50);
-            int actual = radio.getCurrentVolume();
-
-            Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
+    @Test  // ПРОВЕРКА ПРИБАВЛЕНИЯ ГРОМКОСТИ
+
+    public void testIncreaseVolume() {
+        Radio radio = new Radio();
+
+        int expected = 51;
+        radio.setCurrentVolume(50);
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
     }
+
+    @Test  // ПРОВЕРКА УМЕНЬШЕНИЯ ГРОМКОСТИ
+
+    public void testReduceVolume() {
+        Radio radio = new Radio();
+
+        int expected = 49;
+        radio.setCurrentVolume(50);
+        radio.reduceVolume();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test // ПРОВЕРКА ВЫСТАВЛЕНИЯ СТАНЦИИ
+
+    public void shouldSetStation () {
+        Radio radio = new Radio();
+
+        int expected = 10;
+        radio.setCurrentStation(10);
+        int actual = radio.currentStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
